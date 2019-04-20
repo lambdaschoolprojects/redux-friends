9f151a501fe6b761fae5a227e2bbec77dd169d95
RaneWallin <rane@ranewallin.com>
2019-04-20 10:54:26 -0700

    setting up combineReducer

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {applyMiddleware, createStore} from "redux";
-import { Provider } from 'react-redux';
-import {rootReducer} from "./reducers";
-import thunk from 'redux-thunk';
-import reduxLogger from 'redux-logger';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { loginReducer, friendsReducer } from "./reducers";
+import thunk from "redux-thunk";
+import reduxLogger from "redux-logger";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const store = createStore(rootReducer,
-    applyMiddleware(thunk, reduxLogger));
+const rootReducer = combineReducers({ loginReducer, friendsReducer });
 
-ReactDOM.render(<Router><Provider store={store}><App /></Provider></Router>, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, reduxLogger));
 
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,25 +1,35 @@
-import { LOGIN_STARTED, LOGIN_FAIL, LOGIN_SUCCESS } from "../actions";
+import {
+  LOGIN_STARTED,
+  LOGIN_FAIL,
+  LOGIN_SUCCESS,
+  FRIENDS_FETCH_FAILED,
+  FRIENDS_FETCHED,
+  FETCHING_FRIENDS
+} from "../actions";
 
 const initialState = {
-    friends: [],
-    isLoggingIn: false,
-    loginError: ""
-}
+  friends: [],
+  isLoggingIn: false,
+  loginError: ""
+};
 
-export const rootReducer = (state = initialState, action) => {
-    switch(action.type) {
-        case LOGIN_STARTED:
-            return { ...state, isLoggingIn: true };
-            break;
-        case LOGIN_SUCCESS:
-            return { ...state, isLoggingIn: false }
-            break;
-        case LOGIN_FAIL:
-            return { ...state,  isLoggingIn: false, loginError: action.payload }
-            break;
-        default:
-    }
+export const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_STARTED:
+      return { ...state, isLoggingIn: true };
+      break;
+    case LOGIN_SUCCESS:
+      return { ...state, isLoggingIn: false };
+      break;
+    case LOGIN_FAIL:
+      return { ...state, isLoggingIn: false, loginError: action.payload };
+      break;
+    default:
+  }
 
-    return state;
+  return state;
+};
 
-}
+export const friendsReducer = (state = initialState, action) => {
+  return state;
+};
